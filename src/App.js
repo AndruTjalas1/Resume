e4fa3715cb2834dc343e38ecb24482f7a97b8613
AndruTjalas1 <andrutjalas@gmail.com>
2026-03-31 12:06:41 -0700

    fixed routing issue

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import Projects from './pages/Projects';
@@ -9,7 +9,7 @@ function App() {
   const [navOpen, setNavOpen] = useState(false);
 
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <div className="App">
         <nav className="navbar">
           <div className="nav-container">
